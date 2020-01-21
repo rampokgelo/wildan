05719e1c46bd3b93b86acbc8416e03c9b160dd63
rampokgelo <wakeup_wild@yahoo.com>
2020-01-21 10:14:54 +0700

    Updates

---
+++ precache-manifest.3702ad6c8a804560af1a01b2dfbafb69.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8148124223993f6024fc569f2cbb34",
+    "url": "rampokgelo.github.io/wildan/index.html"
+  },
+  {
+    "revision": "b1c747ba6d229bf9b4c7",
+    "url": "rampokgelo.github.io/wildan/static/css/main.0c30417e.chunk.css"
+  },
+  {
+    "revision": "6d17b0ea4c3eca5169d8",
+    "url": "rampokgelo.github.io/wildan/static/js/2.fee87c1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "rampokgelo.github.io/wildan/static/js/2.fee87c1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1c747ba6d229bf9b4c7",
+    "url": "rampokgelo.github.io/wildan/static/js/main.1231664c.chunk.js"
+  },
+  {
+    "revision": "7938bb31cc20fb1ec743",
+    "url": "rampokgelo.github.io/wildan/static/js/runtime-main.4106505b.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "rampokgelo.github.io/wildan/static/media/fa-solid-900.c2801fb4.svg"
+  }
+]);
