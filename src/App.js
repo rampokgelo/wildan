26df30913d7869c89a8ac790381654031bc52547
rampokgelo <wakeup_wild@yahoo.com>
2020-03-21 08:23:02 +0700

    update font awesome and router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,103 @@
-import React from 'react';
-import './App.css';
-import './scss/corestyle.scss';
-import HomeScreen from './component/HomeScreen';
+import React from "react";
+import "./App.css";
+import "./scss/corestyle.scss";
+import HomeScreen from "./component/HomeScreen";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <HomeScreen />
-    </>
+    <Router>
+      <div>
+        <nav className="navbar navbar-main">
+          <div className="wrapper-nav">
+            <div>
+              <a className="navbar-brand" href="/">
+                Brand
+              </a>
+              <ul className="list-unstyled">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/work">
+                    Work
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="posrel navbar-footer">
+              <div className="copyright">
+                © 2019,Brand Co. All rights reserved.
+              </div>
+              <div className="sosmed">
+                <a target="_blank" rel="noopener noreferrer" href="https://id.linkedin.com/in/wildan-zulkarnaen-0627bb73">
+                  <i className="fa fa-facebook-square"/>
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="https://id.linkedin.com/in/wildan-zulkarnaen-0627bb73">
+                  <i className="fa fa-linkedin-square" />
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="https://id.linkedin.com/in/wildan-zulkarnaen-0627bb73">
+                  <i className="fa fa-angellist" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+        <div className="wrapper">
+          <Switch>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/work">
+              <Work />
+            </Route>
+            <Route path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Work() {
+  return <h2>Work</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
 }
 
 export default App;
--- src/component/HomeScreen.js
+++ src/component/HomeScreen.js
@@ -4,124 +4,58 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-main">
-          <div className="wrapper-nav">
-            <div>
-              <a className="navbar-brand" href="/">
-                Brand
-              </a>
-              <ul className="list-unstyled">
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    About us
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Work
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    href="/"
-                    tabindex="-1"
-                    aria-disabled="true"
-                  >
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="posrel navbar-footer">
-              <div className="copyright">
-                © 2019,Brand Co. All rights reserved.
-              </div>
-              <div className="sosmed">
-                <a href="/">
-                  <i className="fab fa-facebook"></i>
-                </a>
-                <a href="/">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href="/">
-                  <i className="fab fa-instagram"></i>
-                </a>
-              </div>
-            </div>
+        <div className="container">
+          <div className="banner">
+            <h1>
+              Let's make it
+              <br />
+              creative.
+            </h1>
           </div>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </nav>
-
-        <div className="wrapper">
-          <div className="container">
-            <div className="banner">
-              <h1>
-                Let's make it
-                <br />
-                creative.
-              </h1>
+          <section>
+            <div className="row">
+              <div className="col-12 col-sm-4">
+                <h3 className="title">
+                  <span>About us</span>
+                </h3>
+              </div>
+              <div className="col-12 col-sm-8">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
             </div>
+          </section>
 
-            <section>
-              <div className="row">
-                <div className="col-12 col-sm-4">
-                  <h3 className="title">
-                    <span>About us</span>
-                  </h3>
-                </div>
-                <div className="col-12 col-sm-8">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </p>
-                </div>
+          <section>
+            <div className="row">
+              <div className="col-12 col-sm-4">
+                <h3 className="title">
+                  <span>Work</span>
+                </h3>
               </div>
-            </section>
-
-            <section>
-              <div className="row">
-                <div className="col-12 col-sm-4">
-                  <h3 className="title">
-                    <span>Work</span>
-                  </h3>
-                </div>
-                <div className="col-12 col-sm-8">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </p>
-                </div>
+              <div className="col-12 col-sm-8">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
               </div>
-              {/* <div className="artwork mt-5">
+            </div>
+            {/* <div className="artwork mt-5">
                 <a
                   href="modal/barnival.html"
                   type="button"
@@ -193,30 +127,29 @@ class HomeScreen extends React.Component {
                   <img src="img/barni.jpg" alt="barnival" />
                 </a>
               </div> */}
-            </section>
+          </section>
 
-            <section>
-              <div className="row">
-                <div className="col-12 col-sm-4">
-                  <h3 className="title">
-                    <span>Contact</span>
-                  </h3>
-                </div>
-                <div className="col-12 col-sm-8">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </p>
-                </div>
+          <section>
+            <div className="row">
+              <div className="col-12 col-sm-4">
+                <h3 className="title">
+                  <span>Contact</span>
+                </h3>
               </div>
-            </section>
-          </div>
+              <div className="col-12 col-sm-8">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+            </div>
+          </section>
         </div>
       </React.Fragment>
     );
