2d3ea1d16a42ad554d5dad0fa44c85eda74e2959
rampokgelo <wakeup_wild@yahoo.com>
2020-03-25 10:10:45 +0700

    updated works

--- src/component/works/WorksData.js
+++ src/component/works/WorksData.js
@@ -23,14 +23,6 @@ export const WORKIMAGES = [
     showcase: require("../../images/porto/popmovie-showcase.jpg"),
     desc:"Character design for short movie animation"
   },
-  // {
-  //   id: 3,
-  //   title: "Asita Annual Report",
-  //   category:"Book",
-  //   images: require("../../images/porto/asita-annual.jpg"),
-  //   showcase: require("../../images/porto/asita-showcase.jpg"),
-  //   desc:"This annual report for the Asita is designed to update members about the company's progress and yearly events.The dynamic color company scheme is used to continue their brand recognition. Visually appealing layout and infographics are used throughout the publication."
-  // },
   {
     id: 3,
     title: "Barnival Indonesia",
@@ -62,6 +54,142 @@ export const WORKIMAGES = [
     images: require("../../images/porto/dbinterior.jpg"),
     showcase: require("../../images/porto/dbinterior-showcase.jpg"),
     desc:"Elegant concept shape, color and sophicasted finishing printing make the name card really love to keep by DB client"
+  },
+  {
+    id: 7,
+    title: "Fit Team",
+    category:"Brand design",
+    images: require("../../images/porto/fitteam.jpg"),
+    showcase: require("../../images/porto/fitteam-showcase.jpg"),
+    desc:"Logo concept is taken from initial brand FT and main brand identity is using cutting shape in left top of side every stationary"
+  },
+  {
+    id: 8,
+    title: "iCashloans",
+    category:"Mobile app",
+    images: require("../../images/porto/icashloans.jpg"),
+    showcase: require("../../images/porto/icashloans-showcase.jpg"),
+    desc:"An online loan money mobile application. You just fill the form and then voila you can loan money up to USD 1000 in 2 minutes."
+  },
+  {
+    id: 9,
+    title: "JournalWays",
+    category:"Web design",
+    images: require("../../images/porto/journalway.jpg"),
+    showcase: require("../../images/porto/journalway-showcase.jpg"),
+    desc:"Web application to share your diary with people you know and you love with, then you can print out album foto via this web application."
+  },
+  {
+    id: 10,
+    title: "Monster",
+    category:"Character design",
+    images: require("../../images/porto/monster.jpg"),
+    showcase: require("../../images/porto/monster-showcase.jpg"),
+    desc:"Prototype for animation character popview"
+  },
+  {
+    id: 11,
+    title: "Mytherapist",
+    category:"Mobile app",
+    images: require("../../images/porto/mytheraphist.jpg"),
+    showcase: require("../../images/porto/mytheraphist-showcase.jpg"),
+    desc:"An app which tell about your feeling everyday and then the app will calculate the problem to resolve bad feel"
+  },
+  {
+    id: 12,
+    title: "Piciocchi Investments",
+    category:"Brand design",
+    images: require("../../images/porto/picio.jpg"),
+    showcase: require("../../images/porto/picio-showcase.png"),
+    desc:"Consulting finance company, their main target audience is high end class people"
+  },
+  {
+    id: 13,
+    title: "QuieroUnCel",
+    category:"Brand design",
+    images: require("../../images/porto/quierouncel.png"),
+    showcase: require("../../images/porto/quierouncel-showcase.jpg"),
+    desc:"Fun design and bright color is to make all class from target audience interesting with this brand"
+  },
+  {
+    id: 14,
+    title: "Zombie",
+    category:"Illustration",
+    images: require("../../images/porto/zombie.jpg"),
+    showcase: require("../../images/porto/zombie-showcase.jpg"),
+    desc:"Just for fun, my passion suddenly come up to create vector illustration design"
+  },
+  {
+    id: 15,
+    title: "RPM",
+    category:"Web design",
+    images: require("../../images/porto/rpm.jpg"),
+    showcase: require("../../images/porto/rpm-showcase.jpg"),
+    desc:"RPM offers professional real estate services and solutions. Our divisions include commercial and residential real estate sales and leasing, property management services, real estate development, real estate investment and auction services."
+  },
+  {
+    id: 16,
+    title: "Shop Rewards",
+    category:"Web design",
+    images: require("../../images/porto/shopreward.jpg"),
+    showcase: require("../../images/porto/shopreward-showcase.jpg"),
+    desc:"E-commerce site using colorfull concept"
+  },
+  {
+    id: 17,
+    title: "SpeedELoans",
+    category:"Web design",
+    images: require("../../images/porto/speedeloans.jpg"),
+    showcase: require("../../images/porto/speedeloans-showcase.jpg"),
+    desc:"An online loan money website."
+  },
+  {
+    id: 18,
+    title: "Top Test Prep",
+    category:"Web design",
+    images: require("../../images/porto/toptestprep.jpg"),
+    showcase: require("../../images/porto/toptestprep-showcase.jpg"),
+    desc:"An online course for student"
+  },
+  {
+    id: 19,
+    title: "Vengoz",
+    category:"Web & Brand design",
+    images: require("../../images/porto/vengoz.jpg"),
+    showcase: require("../../images/porto/vengoz-showcase.jpg"),
+    desc:"Redesign and rebranding website e-commerce website"
+  },
+  {
+    id: 20,
+    title: "World at War",
+    category:"Digital imaging",
+    images: require("../../images/porto/war.jpg"),
+    showcase: require("../../images/porto/war-showcase.jpg"),
+    desc:"Just for fun"
+  },
+  {
+    id: 21,
+    title: "Personal Wedding",
+    category:"Web design",
+    images: require("../../images/porto/wc.jpg"),
+    showcase: require("../../images/porto/wc-showcase.jpg"),
+    desc:"Beautiful story about the groom and the bride. You can see and watch pre-wedding photo and video. Disney world concept make this site feel more than site, it's feel you read book about prince and princess falling in love."
+  },
+  {
+    id: 22,
+    title: "Zapper",
+    category:"Web design",
+    images: require("../../images/porto/zapper.jpg"),
+    showcase: require("../../images/porto/zapper-showcase.jpg"),
+    desc:"Zapper product is QR code generator"
+  },
+  {
+    id: 23,
+    title: "Assalam",
+    category:"Mobile app",
+    images: require("../../images/porto/assalam.jpg"),
+    showcase: require("../../images/porto/assalam-showcase.jpg"),
+    desc:"Full scale project colloborate with my firend, Assalam is muslim application to strengthen ukhuwah, worship, service, communication as well as the economy of pilgrims and Muslims in general. "
   }
 ];
 
