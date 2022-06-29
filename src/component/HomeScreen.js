bbc08946b97129b3860e59be4d839beccb5ed402
rampokgelo <wakeup_wild@yahoo.com>
2022-06-29 13:44:24 +0700

    update home

--- src/component/HomeScreen.js
+++ src/component/HomeScreen.js
@@ -16,48 +16,6 @@ class HomeScreen extends React.Component {
               />
           </h1>
         </div>
-
-        <section>
-          <div className="row">
-            <div className="col-12 col-sm-4">
-              <h3 className="title">
-                <span>About us</span>
-              </h3>
-            </div>
-            <div className="col-12 col-sm-8">
-              <p className="text-white-50">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
-          </div>
-        </section>
-
-        <section>
-          <div className="row">
-            <div className="col-12 col-sm-4">
-              <h3 className="title">
-                <span>Work</span>
-              </h3>
-            </div>
-            <div className="col-12 col-sm-8">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
-          </div>
-        </section>
       </React.Fragment>
     );
   }
